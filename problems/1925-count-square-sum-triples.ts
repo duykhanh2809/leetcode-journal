d7505b89bb213fb644ac29027a3ecf58aa15699c
duykhanh2809 <duykhanh2809as@gmail.com>
2025-12-08 23:08:11 +0700

    1925. Count Square Sum Triples

---
+++ problems/1925-count-square-sum-triples.ts
@@ -0,0 +1,16 @@
+// 1925. Count Square Sum Triples
+
+function countTriples(n: number): number {
+  let res = 0;
+  // enumerate a and b
+  for (let a = 1; a <= n; a++) {
+    for (let b = 1; b <= n; b++) {
+      // determine if it meets the requirements
+      let c = Math.floor(Math.sqrt(a * a + b * b + 1));
+      if (c <= n && c * c === a * a + b * b) {
+        res++;
+      }
+    }
+  }
+  return res;
+}
