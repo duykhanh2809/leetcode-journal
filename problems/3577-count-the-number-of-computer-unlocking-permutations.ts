4e82002249b15c2c708ea2edc8f1a4ba41c9e08d
duykhanh2809 <duykhanh2809as@gmail.com>
2025-12-10 23:06:10 +0700

    3577. Count the Number of Computer Unlocking Permutations

---
+++ problems/3577-count-the-number-of-computer-unlocking-permutations.ts
@@ -0,0 +1,15 @@
+function countPermutations(complexity: number[]): number {
+  const n = complexity.length;
+  for (let i = 1; i < n; i++) {
+    if (complexity[i] <= complexity[0]) {
+      return 0;
+    }
+  }
+
+  let ans = 1;
+  const mod = 1000000007;
+  for (let i = 2; i < n; i++) {
+    ans = (ans * i) % mod;
+  }
+  return ans;
+}
